6127ed54742bc1743f5f4f90b8b380fb270f0bd2
Haritha321215 <nithyaprerana@gmail.com>
2024-05-08 01:07:32 -0500

    Started the server

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-	// import the mongoose module 
-	const mongoose = require('mongoose');
-	// import the config file
-	const config = require("./utils/config")
-	// console.log the message Connecting to MongoDB 
-	console.log('Connecting to MongoDB');
-	// create the database
-	mongoose.connect(config.MONGODB_URI)
-	    .then(() => {
-	        console.log('Connected to MongoDB');
-	        // start the server
-	        // listen to the port
-	        // app.listen(config.PORT, () => {
-	        //      console.log(`Server running on port ${config.PORT}`);
-	        // });
-	    })
-	    .catch((error) => {
-	        console.log('Error connecting to MongoDB:', error.message);
-     });
+// import the mongoose module
+const mongoose = require("mongoose");
+
+// import the config module
+const config = require("./utils/config");
+
+// import the app module
+const app = require("./app");
+
+// console.log the message Connecting to MongoDB
+console.log("Connecting to MongoDB...");
+
+// connect to the database
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+
+    // start the server
+    // listen to the port
+    app.listen(config.PORT, () => {
+      console.log(`Server running on port ${config.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB:", error.message);
+  });
