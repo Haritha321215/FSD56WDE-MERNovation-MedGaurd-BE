8f9ce7ca4eab4f961051e657ed4604919878e1f1
Haritha321215 <nithyaprerana@gmail.com>
2024-05-09 01:13:56 -0500

    updated url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 //enable cors requests from http://localhost:5173
 app.use(
   cors({
-    origin: "https://fsd56wde-mernovation-medgaurd-fe.netlify.app/", 
+    origin: "https://fsd56wde-mernovation-medgaurd-fe.netlify.app", 
     credentials: true,
   })
 );
